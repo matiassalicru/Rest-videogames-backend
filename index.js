d73105b0dc888f0639f9c112453db2773e73d9e9
Matias Salicru <mattiassalicru@gmail.com>
2021-04-04 07:50:43 -0300

    dotenv added

--- index.js
+++ index.js
@@ -1,40 +1,47 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
 const { default: axios } = require("axios");
 
+const app = express();
+const envFound = dotenv.config();
+
+app.use(cors());
+
+const port = 3000;
+let auth = "no secret key";
+if (envFound) {
+  auth = process.env.SECRET_KEY;
+}
+
 console.log(
+  auth,
   "hello world $$$$$$$$$$$$$$$$$$$$$$$$############################$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
 );
+
+const data = [];
 
 axios({
   url: "https://api.igdb.com/v4/covers",
   method: "POST",
   headers: {
     "Client-ID": "gillmyg4he2iqgf9dxmlpn97f7em22",
-    Authorization: "Bearer ydpawzo4ydflwtvhb86notvyb72lpj",
+    Authorization: `${auth}`,
   },
-  data: "fields alpha_channel,animated,checksum,game,height,image_id,url,width;",
+  data: "fields game,height,image_id,url,width; limit 10;",
 })
   .then((res) => {
     console.log(res.data);
+    data.push(res.data);
   })
   .catch((e) => {
     console.log(e);
   });
 
-// axios
-//   .post(
-//     "https://api.igdb.com/v4/covers",
-//     { data: 'id'
-//      },
-//     {
-//       headers: {
-//         "Client-ID": "gillmyg4he2iqgf9dxmlpn97f7em22",
-//         Authorization: "Bearer ydpawzo4ydflwtvhb86notvyb72lpj",
-//       },
-//     }
-//   )
-//   .then((res) => {
-//     console.log(res.data);
-//   })
-//   .catch((e) => {
-//     console.log(e);
-//   });
+app.get("/", (req, res) => {
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log(`server on port: ${port}`);
+});
